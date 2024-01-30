d294e517bcb1b6375a1247113a1149e6a39df32f
Sasha <georgefbrockman@gmail.com>
2024-01-30 15:51:00 -0600

    finishes lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,11 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_traverse = [self.root]
+    while len(nodes_to_traverse) > 0:
+      node = nodes_to_traverse.pop(0)
+      if node['id'] == id:
+        return node 
+      else:
+        nodes_to_traverse = node["children"] + nodes_to_traverse
+    return None
